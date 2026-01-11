400dc76859ef69dd08f99bfa976f3fa176f9b01f
DeviaDev <devianestnarendra@gmail.com>
2026-01-11 20:41:58 +0700

    Update Login

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -14,6 +14,6 @@ class Login extends BaseLogin
 
     public function getSubheading(): string|Htmlable|null
     {
-        return 'Silakan masuk ke sistem';
+        return 'Silakan login ke sistem';
     }
 }
